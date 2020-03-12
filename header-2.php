e9172cc5fc932e3ff3a29bfc7e35d19a272bae8e
binhhd <onghoan2009@gmail.com>
2020-03-12 17:02:49 +0700

    update url

--- header-2.php
+++ header-2.php
@@ -17,13 +17,13 @@
 
 <body <?php body_class(); ?>>
 <div style="background-color: #C40504; box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.1);backdrop-filter: blur(10px); height: 85px; width: 100%">
-    <a href="http://192.168.0.113/aeon/"><div style="background: url('http://localhost/hb/wp-content/uploads/screenshots/Logo-02%203.png'); width: 229px; height: 38px; position: relative; left: 12%; top: 24px">
+    <a href="<?php echo(get_home_url())?>"><div style="background: url('<?php echo(get_home_url() . '/wp-content/themes/vendd/screenshots/Logo-02%203.png')?>'); width: 229px; height: 38px; position: relative; left: 12%; top: 24px">
     </div></a>
     <ul style="list-style: none; color: #FFF; display: flex; flex-direction: row; float: right; margin-right: 160px">
         <li style="margin-left: 50px; font-family: SVN-Gotham;font-style: normal;font-weight: 500;font-size: 16px;line-height: 25px;letter-spacing: 0.04em;color: #FFFFFF;">Login | Register</li>
         <li style="background: url('http://localhost/hb/wp-content/uploads/screenshots/notifications_24px_outlined.svg'); width: 24px; height: 24px; margin-left: 50px"></li>
         <a href="<?php echo edd_get_checkout_uri(); ?>">
-            <li style="background: url('http://192.168.0.113/hb/wp-content/uploads/screenshots/Cart.png'); width: 16px; height: 16px; margin-left: 50px; position: relative; top: 5px">
+            <li style="background: url('<?php echo(get_home_url() . '/wp-content/themes/vendd/screenshots/Cart.png')?>'); width: 16px; height: 16px; margin-left: 50px; position: relative; top: 5px">
             </li>
         </a>
 
